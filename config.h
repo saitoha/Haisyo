0fa985a245bd67dbac9a136602a678da336f87fe
Hayaki Saito <user@zuse.jp>
2014-02-14 22:09:48 +0900

    Introduce autoconf build

---
+++ config.h
@@ -0,0 +1,36 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "user@zuse.jp"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "haisyo"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "haisyo 0.2.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "haisyo"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2.0"
+
+/* Define */
+/* #undef HAVE_DSTERASE */
+
+/* Define */
+#define HAVE_SHGETFOLDERPATH 1
+
+/* Define */
+/* #undef HAVE_SHGETSPECIALFOLDERPATH */
+
+/* windows 2000 */
+#define WINVER 0x0500
+#define _WIN32_WINNT 0x0500
+
+/* Internet Explorer 4.0 */
+#define _WIN32_IE    0x0400
