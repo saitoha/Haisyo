61bf6d819d2006881bbc4edbb7a4a9b988aad63a
saitoha <user@zuse.jp>
2014-04-19 19:08:24 +0900

    Rebuild configure script

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define PACKAGE_NAME "haisyo"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "haisyo 1.2.0"
+#define PACKAGE_STRING "haisyo 1.3.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "haisyo"
@@ -17,7 +17,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.0"
+#define PACKAGE_VERSION "1.3.0"
 
 /* Define */
 /* #undef HAVE_DSTERASE */
